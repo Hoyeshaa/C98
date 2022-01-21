850241d1ed909758239139d20fb8e5f187146ac7
Hoyeshaa <92789345+Hoyeshaa@users.noreply.github.com>
2022-01-21 15:27:10 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var stot = window.webkitSpeechRecognition;
 var spt = new stot();
 function start(){
     document.getElementById("textbox").innerHTML = "";
+    spt.lang = "fr";
     spt.start();
 }
 spt.onresult = function run(event){
@@ -9,4 +10,19 @@ spt.onresult = function run(event){
     result = event.results[0][0].transcript
     console.log(result)
     document.getElementById("textbox").innerHTML = result;
+    speak();
 }
+function speak(){
+    sp = window.speechSynthesis;
+    data = document.getElementById("textbox").value;
+    utter = new SpeechSynthesisUtterance(data);
+    sp.speak(utter);
+    Webcam.attach(cam);
+}
+Webcam.set({
+    width:360,
+    height:250,
+    image_format:'png',
+    png_quality:100
+})
+cam = document.getElementById("camera");
