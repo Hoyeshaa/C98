a3507f468d4f0d47a315c88545a9423013bba624
Hoyeshaa <92789345+Hoyeshaa@users.noreply.github.com>
2022-01-31 15:39:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ var stot = window.webkitSpeechRecognition;
 var spt = new stot();
 function start(){
     document.getElementById("textbox").innerHTML = "";
-    spt.lang = "fr";
     spt.start();
 }
 spt.onresult = function run(event){
@@ -10,14 +9,20 @@ spt.onresult = function run(event){
     result = event.results[0][0].transcript
     console.log(result)
     document.getElementById("textbox").innerHTML = result;
-    speak();
+    if(result=="take my selfie"){
+        speak();
+    }
 }
 function speak(){
     sp = window.speechSynthesis;
-    data = document.getElementById("textbox").value;
+    data = "taking your selfie in 5 seconds";
     utter = new SpeechSynthesisUtterance(data);
     sp.speak(utter);
     Webcam.attach(cam);
+    setTimeout(function(){
+        pics();
+        save();
+    },5000)
 }
 Webcam.set({
     width:360,
@@ -26,3 +31,14 @@ Webcam.set({
     png_quality:100
 })
 cam = document.getElementById("camera");
+function pics(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='pic'src='"+data_uri+"'>";
+    })
+}
+function save(){
+   link =  document.getElementById("link");
+   src = document.getElementById("pic").src;
+   link.href = src;
+   link.click();
+}
